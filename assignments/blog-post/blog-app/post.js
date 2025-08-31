ebe2e6248211cd407362f9ac4642ac917c3da4ae
BrandonT <Btiseo@uncc.edu>
2025-08-31 13:48:46 -0400

    initial commit of blog app

---
+++ assignments/blog-post/blog-app/post.js
@@ -0,0 +1,30 @@
+// post.js
+// This module handles blog post operations (add, list, view, update, delete)
+
+function addPost(title, content) {
+  // TODO: Implement logic to add a new post and save it
+}
+
+function listPosts() {
+  // TODO: Implement logic to return all posts
+}
+
+function viewPost(id) {
+  // TODO: Implement logic to find and return a post by ID
+}
+
+function updatePost(id, newTitle, newContent) {
+  // TODO: Implement logic to update a post
+}
+
+function deletePost(id) {
+  // TODO: Implement logic to delete a post
+}
+
+module.exports = {
+  addPost,
+  listPosts,
+  viewPost,
+  updatePost,
+  deletePost,
+};
