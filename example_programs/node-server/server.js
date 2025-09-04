b2c1001895175058d10ca6bb89da3c9dd4b6f42f
BrandonTiseo <Brandontiseo@yahoo.com>
2025-09-03 23:25:54 -0400

    final commit of node-server

--- example_programs/node-server/server.js
+++ example_programs/node-server/server.js
@@ -5,29 +5,51 @@ import fs from 'fs/promises';
 import {join} from 'path';
 
 const port = 8080; //use a port that isnt used already.
-const filePath = join(import.meta.dirname, 'public', 'index.html');
 
 //callback function called everytime we get a request from the client side.
 const server = http.createServer( async (req, res)=>{
+    /*
     console.log(req.method);
     console.log(req.url);
-    /*
     //res.statusCode = 200;
     //res.setHeader('Content-Type','text/plain');
     res.writeHead(200, {'Content-Type': 'text/html'}); //combines above two lines. Second param defines what we send in write.
     res.write('<h1>Hello world</h1>'); //can do multiple .write lines.
     res.end(); //ends response and sends above data to client.
     */
+    let url = req.url;
+    if(req.url === '/') url = 'index.html';
+    const filePath = join(import.meta.dirname, 'public', url);
+    let fileType = url.split('.').pop();
 
-    //send html document.
+    let contentType = '';
+
+    switch(fileType){
+        case 'html':
+        case'css':
+        case 'javascript':
+            contentType = `text/${fileType}`;
+            break;
+        case 'txt':
+            contentType = `text/plain`;
+            break;
+        case 'json':
+            contentType = `application/json`;
+            break;
+        case 'jpeg':
+        case 'jpg':
+            contentType = `image/jpeg`;
+            break;
+    }
+
     try{
         const data = await fs.readFile(filePath);
-        res.writeHead(200, {'Content-Type':'text/html'});
+        res.writeHead(200, {'Content-Type': contentType});
         res.end(data);
     } catch(error){
         console.log(error);
         res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end('<h1>Not FOund</h1>');
+        res.end('<h1>Not Found</h1>');
     }
 });
 
