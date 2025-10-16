15835d43c15df291ede5aca34877a0b029aec33f
BrandonTiseo <Brandontiseo@yahoo.com>
2025-10-16 09:38:50 -0400

    added signup validation

--- assignments/assignment_7/module8-starter/src/routes/authRoutes.js
+++ assignments/assignment_7/module8-starter/src/routes/authRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import { signUpHandler } from '../controllers/authController.js';
+import {validateUser} from '../middleware/userValidators.js';
 const router = express.Router();
 
-router.post('/signup', signUpHandler);
+router.post('/signup', validateUser, signUpHandler);
 
 export default router;
