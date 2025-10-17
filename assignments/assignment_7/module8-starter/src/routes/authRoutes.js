18ec25d1f084a7a20e90ae9925e9830dfa48739b
BrandonTiseo <Brandontiseo@yahoo.com>
2025-10-17 12:10:18 -0400

    added rate limiter

--- assignments/assignment_7/module8-starter/src/routes/authRoutes.js
+++ assignments/assignment_7/module8-starter/src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { signUpHandler, logInHandler } from '../controllers/authController.js';
 import {validateUser} from '../middleware/userValidators.js';
+import LogInLimiter from '../middleware/rateLimiter.js';
 const router = express.Router();
 
 router.post('/signup', validateUser, signUpHandler);
 
-router.post('/login', validateUser, logInHandler);
+router.post('/login', LogInLimiter, validateUser, logInHandler);
 
 export default router;
